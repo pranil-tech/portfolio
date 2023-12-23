dc9de8c5b5e4366da280b0458bafe1acbde02520
pranil-tech <praneel0905@gmail.com>
2023-12-23 23:24:28 +0530

    content 2.0

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const educationGrid = document.querySelector('.education-grid');
-    const skillsGrid = document.querySelector('.skills-grid');
 
-    // Function to show the education grid
-    function showEducationGrid() {
-        educationGrid.style.display = 'grid';
-        skillsGrid.style.display = 'none';
-    }
+function showEducation() {
+    document.getElementById('education-container').style.display = 'block';
+    document.getElementById('internship-container').style.display = 'none';
+}
 
-    // Function to show the skills grid
-    function showSkillsGrid() {
-        educationGrid.style.display = 'none';
-        skillsGrid.style.display = 'grid';
-    }
+function showInternships() {
+    document.getElementById('education-container').style.display = 'none';
+    document.getElementById('internship-container').style.display = 'block';
+}
 
-    // Attach click event listeners to your education and skills triggers
-    const educationTrigger = document.querySelector('#education-trigger');
-    const skillsTrigger = document.querySelector('#skills-trigger');
-
-    educationTrigger.addEventListener('click', showEducationGrid);
-    skillsTrigger.addEventListener('click', showSkillsGrid);
-});
+function showSkills() {
+    document.getElementById('education-container').style.display = 'none';
+    document.getElementById('internship-container').style.display = 'none';
+}
