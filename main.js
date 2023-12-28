1efc7da9bbf105c7af9032ac87d312232c551af3
pranil-tech <praneel0905@gmail.com>
2023-12-28 09:34:26 +0530

    footer

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ function toggleSection(section) {
     const educationContainer = document.getElementById('education-container');
     const internshipContainer = document.getElementById('internship-container');
 
-    educationContainer.style.display = section === 'education' ? 'flex' : 'none';
-    internshipContainer.style.display = section === 'internship' ? 'flex' : 'none';
+    if (section === 'education') {
+        educationContainer.style.display = 'flex';
+        internshipContainer.style.display = 'none';
+    } else if (section === 'internship') {
+        educationContainer.style.display = 'none';
+        internshipContainer.style.display = 'flex';
+    }
 }
