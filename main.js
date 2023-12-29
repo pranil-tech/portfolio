228c60877bfb51ab1febae2c433c88686020562b
pranil-tech <praneel0905@gmail.com>
2023-12-29 22:28:15 +0530

    styling 3.0

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
-function toggleSection(section) {
-    const educationContainer = document.getElementById('education-container');
-    const internshipContainer = document.getElementById('internship-container');
+document.addEventListener("DOM Loaded",function(){
+    document.getElementById("education-button").click();
+});
+
+
+function toggle(section) {
+    var education = document.getElementById("education-container");
+    var internship = document.getElementById("internship-container");
+
+    var educationButton = document.getElementById("education-button");
+    var internshipButton = document.getElementById("internship-button");
 
     if (section === 'education') {
-        educationContainer.style.display = 'flex';
-        internshipContainer.style.display = 'none';
-    } else if (section === 'internship') {
-        educationContainer.style.display = 'none';
-        internshipContainer.style.display = 'flex';
+        education.style.display = "flex";
+        internship.style.display = "none";
+
+        // Update button styling
+        educationButton.classList.add("active");
+        internshipButton.classList.remove("active");
+    } else {
+        education.style.display = "none";
+        internship.style.display = "flex";
+
+        // Update button styling
+        educationButton.classList.remove("active");
+        internshipButton.classList.add("active");
     }
 }
